996c4844e2f1eef8514ad084cddda2ad6f7ea6a6
Francesco Pretto <ceztko@gmail.com>
2023-03-23 00:44:19 +0100

    [FIX] Found a non deprecated working syntax for test-addondll.js

--- x64/Debug/test-addondll.js
+++ x64/Debug/test-addondll.js
@@ -1,3 +1,9 @@
-var addon = require('.\\addondll.dll')
+const { dlopen } = require('node:process');
+
+console.log('Try load addondll.dll ..')
+
+const mod = { exports: {} };
+process.dlopen(mod, '.\\addondll.dll');
+var addon = mod.exports
 
 console.log('This should be eight:', addon.add(3, 5))
